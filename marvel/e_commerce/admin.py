3f64ed5fb7e22f72501646473951552c637039cc
FranciscoReno <franciscoreno@gmail.com>
2021-08-04 11:09:43 +0200

    nueva tarea

--- marvel/e_commerce/admin.py
+++ marvel/e_commerce/admin.py
@@ -1,34 +1,59 @@
 from django.contrib import admin
 
 # NOTE: Tenemos que importar los modelos con los que vamos a trabajar:
-# from e_commerce.models import *
+from e_commerce.models import *
 
 # Register your models here.
 
 # NOTE: Aquí personalizamos los campos en el Django Admin.
 
-# @admin.register(Comic)
-# class ComicsAdmin(admin.ModelAdmin):
+@admin.register(Comic)
+class ComicsAdmin(admin.ModelAdmin):
     # NOTE: Para seleccionar los campos en la tabla de registros
-    # list_display = ('marvel_id', 'title', 'stock_qty', 'price')
+    list_display = ('marvel_id', 'title', 'stock_qty', 'price')
 
     # NOTE: Filtro lateral de elementos:
-    # list_filter= ('marvel_id','title')
+    list_filter= ('marvel_id','title')
     
     # NOTE: Buscador de elementos en la columna:
-    # search_fields = ['title']
+    search_fields = ['title']
 
-    # NOTE: Para seleccionar los campos en el registro. 
+    # NOTE: Para seleccionar los campos que queremos que el administrativo pueda editar
     # fields = ('marvel_id', 'title', 'stock_qty')
 
     # NOTE: Genera un campo desplegable con los registros seleccionados.
-    # fieldsets = (
-    #     (None, {
-    #         'fields': ('marvel_id', 'title', 'stock_qty')
-    #     }),
-    #     ('Advanced options', {
-    #         'classes': ('collapse',),
-    #         'fields': ('description','price', 'picture'),
-    #     }),
-    # )
-    # pass
+    fieldsets = (
+        (None, {
+            'fields': ('marvel_id', 'title', 'stock_qty')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('description','price', 'picture'),
+        }),
+    )
+
+
+@admin.register(WishList)
+class WishListsAdmin(admin.ModelAdmin):
+    # NOTE: Para seleccionar los campos en la tabla de registros
+    list_display = ('user_id','comic_id', 'cart', 'wished_qty', 'buied_qty')
+
+    # NOTE: Filtro lateral de elementos:
+    list_filter= ('user_id','comic_id')
+    
+    # NOTE: Buscador de elementos en la columna:
+    search_fields = ['comic_id']
+
+    # NOTE: Para seleccionar los campos que queremos que el administrativo pueda editar
+    # fields = ('cart', 'wished_qty', 'buied_qty')
+
+    # NOTE: Genera un campo desplegable con los registros seleccionados.
+    fieldsets = (
+        (None, {
+            'fields': ('user_id','comic_id')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('cart', 'wished_qty', 'buied_qty'),
+        }),
+    )
